f0553cad6ef06f5680457813343098df54ee9a2a
Susilkumar Udhayakumar <87655701+Robobuzz13@users.noreply.github.com>
2025-04-17 18:56:53 +0530

    Update RetryListener.java

--- src/main/java/listeners/RetryListener.java
+++ src/main/java/listeners/RetryListener.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 public class RetryListener implements IRetryAnalyzer {
 
     private int count = 0;
-    private final int MAX_TRY = 3;
+    private final int MAX_TRY = 4;
 
     @Override
     public boolean retry(ITestResult iTestResult) {
